a33df6cd8d760b1ea999618620bd8f8f391ea1c2
Colum Ferry <cferry09@gmail.com>
2022-03-04 10:01:51 +0000

    fix: storybook for svg

--- libs/svg-layout/.storybook/webpack.config.js
+++ libs/svg-layout/.storybook/webpack.config.js
@@ -1,18 +1,20 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+// const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-// Export a function. Accept the base config as the only param.
-module.exports = async ({ config }) => {
-	config.resolve.extensions.push('.svg');
+// // Export a function. Accept the base config as the only param.
+// module.exports = async ({ config }) => {
+//   config.resolve.extensions.push('.svg');
 
-	config.module.rules = config.module.rules.map((data) => {
-		if (/svg\|/.test(String(data.test))) data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
-		return data;
-	});
+//   config.module.rules = config.module.rules.map((data) => {
+//     if (/svg\|/.test(String(data.test)))
+//       data.test =
+//         /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+//     return data;
+//   });
 
-	config.module.rules.push({
-		test: /\.svg$/,
-		use: [{ loader: 'svg-inline-loader' }]
-	});
+//   // config.module.rules.push({
+//   // 	test: /\.svg$/,
+//   // 	use: [{ loader: 'svg-inline-loader' }]
+//   // });
 
-	return config;
-};
+//   return config;
+// };
